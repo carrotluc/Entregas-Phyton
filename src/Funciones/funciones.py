591c3a5728361c3c862c2cfb44fe478b2f0d5617
Lucy <Lucy@10.100.239.130>
2024-10-10 14:11:13 +0200

    a

--- src/Funciones/funciones.py
+++ src/Funciones/funciones.py
@@ -3,16 +3,16 @@ def calcular_producto(n:int, k:int) -> int:
     if n <= k:
         return "ERROR: ¡n debe ser mayor que k!"
     s:int = 1  
-    for i in range(k + 1):  
-        s *= (n - i + 1) 
+    for i in range(0,k + 1):  
+        s = s*(n - i + 1) 
     return s
 
 #ACTIVIDAD 2 - ENTREGA 1:
 def producto_secuencia_geo(a: int, r: int, k:int) -> int:
     s:int = 1
     for i in range(1, k+1): 
-        ai= a* (r** (i-1))
-        s *= ai
+        ai= a*(r** (i-1))
+        s =s * ai
     return s
 
 #ACTIVIDAD 3 - ENTREGA 1: 
@@ -26,13 +26,25 @@ def numero_combinatorio(n:int, k:int) -> int:
 
 #ACTIVIDAD 4 - ENTREGA 1:
 from math import factorial
-def producto_s(n:int, k:int) -> int:
-     if n < k:
+def producto_s(n:int, k:int) -> float:
+    if n < k:
         return "ERROR: ¡n debe ser mayor que k!"
-    s:int = 1  
-    for i in range(1, factorial(k)):  
-        s *= (-1)*i*() _???
-    return s
+    else: 
+        s= 0
+        a: float = 1/factorial(k)
+        for i in range(0,k): #Es hasta k ya que en los rangos el últ no está incluido
+            c: float = (-1)**i
+            b: float = numero_combinatorio(k+1,i+1)
+            d: float = (k-i)**n
+            s:int = s + (a*c*b*d)
+        return s
+
+#ACTIVIDAD 5 - ENTREGA 1:
+from typing import Callable
+#f es la función
+#g es la función derivada
+def m_newton(f: Callable[[int],int], g: Callable[[int],int], a:float, e:float)-> float:
+    if f´(x
     
     
     
--- src/test/TestFunciones.py
+++ src/test/TestFunciones.py
@@ -1,22 +1,60 @@
 ################################################################################
 #EJEMPLO 1:
-n: float =4
-k: float =2
-r = calcular_producto(n,k)
-print(f'Para el caso de n={n} y k={k}, \nla solución del enunciado será {r}.')
+from pygments.lexers import int_fiction
+
+
+def calcular_producto(n:int, k:int) -> int:
+    if n <= k:
+        return "ERROR: ¡n debe ser mayor que k!"
+    s:int = 1  
+    for i in range(0,k + 1):  
+        s = s*(n - i + 1) 
+    return s
+
+def test1(): 
+    n:int = 4
+    k:int = 2
+    r = calcular_producto(n,k)
+    print(f'Para el caso de n={n} y k={k}, \nla solución del enunciado será {r}.')
 ################################################################################
 #EJEMPLO 2:
-ai = 3
-r = 5
-k = 2
-resultado = producto_secuencia_geo(ai, r, k)
-print(f"El producto de los primeros {k} términos es: {resultado}")
+def producto_secuencia_geo(a: int, r: int, k:int) -> int:
+    s:int = 1
+    for i in range(1, k+1): 
+        ai= a*(r** (i-1))
+        s =s * ai
+    return s
+
+def test2(): 
+    a: int = 3
+    r:int = 5
+    k:int = 2
+    resultado = producto_secuencia_geo(a, r, k)
+    print(f"El producto de los primeros {k} términos es: {resultado}")
 ##############################################################################      
 #EJEMPLO 3: 
-n = 4
-k = 2
-sol = numero_combinatorio(n,k)
-print(f'La solución al problema es {sol}')
+from math import factorial
+def numero_combinatorio(n:int, k:int) -> int:
+    if n < k:
+        return "ERROR: ¡n debe ser mayor que k!"
+    else: 
+        nm: int = factorial(n)/(factorial(k)*factorial(n-k)) 
+    return nm
+
+def test3():
+    n: int = 4
+    k: int = 2
+    sol = numero_combinatorio(n,k)
+    print(f'La solución al problema es {sol}')
+###########################################################################################
+#EJEMPLO 4:
+    
+    
+#########################################################################
+if __name__ == '__main__':
+    print(test1())
+    print(test2())
+    print(test3())
 
 
 
